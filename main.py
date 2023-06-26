21a81b14214ccfc9c0a9b513cc5d16787cc2be4e
SreejataBanerjee <112263429+SreejataBanerjee@users.noreply.github.com>
2023-06-26 11:45:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+
+def open_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        with open(file_path, 'r') as file:
+            text_editor.delete('1.0', tk.END)
+            text_editor.insert(tk.END, file.read())
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension='.txt')
+    if file_path:
+        with open(file_path, 'w') as file:
+            file.write(text_editor.get('1.0', tk.END))
+
+root = tk.Tk()
+root.title("Basic Text Editor")
+root.geometry("350x250")
+root.minsize(height=250, width=350)
+
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT,
+               fill=Y)
+text_editor = tk.Text(root, yscrollcommand=scrollbar.set)
+text_editor.pack(fill=BOTH)
+
+  
+
+scrollbar.config(command=text_editor.yview)
+
+menu_bar = tk.Menu(root)
+file_menu = tk.Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+menu_bar.add_cascade(label="File", menu=file_menu)
+
+root.config(menu=menu_bar)
+root.mainloop()
